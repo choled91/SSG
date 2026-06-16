53fedf4be4e4178d0a01dfa0ef6432c45d262deb
choled91 <sadun.choled@gmail.com>
2026-06-16 12:54:00 +0700

    Debuging attempt 3

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-python3 src/main.py "/SSG"
+python3 src/main.py "/SSG/"
--- src/extract_title.py
+++ src/extract_title.py
@@ -1,8 +1,5 @@
 def extract_title(markdown):
-    header = ""
-    if markdown.startswith("#") and not markdown.startswith("##"):
-        header +=markdown[1:].replace(" ", "")
-        return header
-    else:
-        raise Exception("No header found")
-    
+    for headline in markdown.split("\n"):
+        if headline.startswith("# "):
+            return headline[2:]
+    raise ValueError("No h1 heading found")
